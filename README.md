006a7e2324788f1776d6f4bb6910666d92c70ff4
Josep Gregori <josepgb13@gmail.com>
2023-05-22 03:45:41 -0400

    Proyecto acabado

---
+++ README.md
@@ -0,0 +1,77 @@
+
+# Pokedex
+
+Pokedex es una aplicación web que muestra información sobre diferentes Pokémon. Permite a los usuarios explorar una lista de Pokémon y realizar búsquedas para encontrar información específica sobre cada uno.
+
+## Características
+
+- Mostrar una lista de Pokémon con sus nombres y características básicas.
+- Permitir realizar búsquedas de Pokémon por nombre.
+- Mostrar detalles completos de un Pokémon seleccionado.
+- Cambiar el tema de la aplicación entre diferentes opciones (Water, Fire, Plant, Dark).
+- Enlace al repositorio de GitHub con el código fuente de la aplicación.
+
+## Tecnologías utilizadas
+
+- React: biblioteca de JavaScript para construir interfaces de usuario.
+- HTML: lenguaje de marcado para estructurar el contenido de la aplicación web.
+- CSS: lenguaje de estilos para dar estilo y diseño a la aplicación.
+- LocalStorage: API del navegador para almacenar el tema seleccionado por el usuario.
+
+## Instalación
+
+1. Clona el repositorio de GitHub:
+
+```
+git clone https://github.com/tu-usuario/pokedex.git
+```
+
+2. Navega al directorio del proyecto:
+
+```
+cd pokedex
+```
+
+3. Instala las dependencias del proyecto:
+
+```
+npm install
+```
+
+4. Inicia la aplicación:
+
+```
+npm run dev
+```
+
+La aplicación estará disponible en tu navegador en la dirección [http://localhost:3000](http://localhost:5173).
+
+## Uso
+
+- Al cargar la aplicación, se mostrará una lista de Pokémon.
+- Puedes realizar una búsqueda de un Pokémon específico ingresando su nombre en el campo de búsqueda.
+- Haz clic en un Pokémon para ver sus detalles completos.
+- Utiliza los botones de tema (Water, Fire, Plant, Dark) para cambiar el aspecto de la aplicación.
+- Haz clic en el enlace del repositorio de GitHub para acceder al código fuente.
+
+## Contribución
+
+Si deseas contribuir a este proyecto, puedes seguir los pasos a continuación:
+
+1. Haz un fork del repositorio.
+2. Crea una rama para tu nueva función: `git checkout -b nueva-funcion`.
+3. Realiza los cambios y realiza los commits: `git commit -m "Agrega nueva función"`.
+4. Envía tus cambios al repositorio remoto: `git push origin nueva-funcion`.
+5. Abre un Pull Request en GitHub.
+
+## Link desplegado.
+[Link pagina](https://badluckxiii.github.io/pokedex)
+
+## Créditos
+
+Este proyecto fue desarrollado por [Josep Gregori](https://github.com/badluckxiii).
+
+## Licencia
+
+Este proyecto está bajo la [Licencia MIT](LICENSE).
+
--- src/Components/NavHeader.jsx
+++ src/Components/NavHeader.jsx
@@ -1,69 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import '../assets/styles/Header.css';
+
 const themes = {
     'Water': '#00BFFF',
     'Fire': '#FF4500',
     'Plant': '#00FF00',
     'Dark': '#333',
-}
+};
 
-const marginBottom = { marginBottom: '20px' }
+const marginBottom = { marginBottom: '20px' };
+
 const NavHeader = () => {
     const [theme, setTheme] = useState(null);
-    const onClickGetKey = (key) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const onClickGetKey = (key) => {
         localStorage.setItem('theme', key);
         setTheme(localStorage.getItem('theme'));
-    }
+    };
+
     useEffect(() => {
-        if (localStorage.getItem('theme') !== undefined) {
+        if (localStorage.getItem('theme')) {
             setTheme(localStorage.getItem('theme'));
         } else {
-
-            localStorage.setItem('theme', 'Dark')
+            localStorage.setItem('theme', 'Dark');
         }
+    }, []);
 
-    }, [])
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <header style={{
-            backgroundColor: themes[theme],
-            height: '75px',
-            alignItems: 'center',
-            display: 'flex'
-        }}>
-            <div className='navigationMenu'>
-                <nav>
-                    <div className="menu-toggle">
-                        <input type="checkbox" name="" id="" />
-
-                    </div>
-                    <ul className="menu">
-
-                        <li>
-                            <a href="#">Pokedex</a>
-                            <ul className="submenu">
-                                <li><button onClick={() => onClickGetKey('Water')} className='buttonNav'>Water</button></li>
-                                <li><button onClick={() => onClickGetKey('Fire')} className='buttonNav'>Fire</button></li>
-                                <li><button onClick={() => onClickGetKey('Plant')} className='buttonNav'>Plant</button></li>
-                                <li style={marginBottom}><button onClick={() => onClickGetKey('Dark')} className='buttonNav'>Dark</button></li>
-                                <li style={marginBottom}><a href="https://github.com/badluckxiii/pokedex" className='buttonNav separator' target='_blank'>Github repository</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-
-            </div>
-            <div style={{
-                width: '30%',
-                textAlign: 'right'
-
-            }}>
-                <input placeholder='Search a pokemon...' style={{ marginRight: '50px', borderRadius: '15px' }} type="text" name="" id="" />
-
+        <header
+            style={{
+                backgroundColor: themes[theme],
+                // height: window.innerWidth < 768 ? '375px' : '75px',
+                flexDirection: window.innerWidth < 768 ? 'column' : 'row',
+                alignItems: 'center',
+                display: 'flex',
+            }}
+        >
+            <nav style={{width: window.innerWidth<768?'100%':'70%', }}>
+                <div className="menu-toggle" onClick={handleToggleMenu}>
+                    <input type="checkbox" name="" id="" />
+                </div>
+                <ul className={`menu `}>
+                    <li>
+                        <a href="#">Pokedex</a>
+                        <ul className="submenu">
+                            <li>
+                                <button onClick={() => onClickGetKey('Water')} className="buttonNav">
+                                    Water
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => onClickGetKey('Fire')} className="buttonNav">
+                                    Fire
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => onClickGetKey('Plant')} className="buttonNav">
+                                    Plant
+                                </button>
+                            </li>
+                            <li style={marginBottom}>
+                                <button onClick={() => onClickGetKey('Dark')} className="buttonNav">
+                                    Dark
+                                </button>
+                            </li>
+                            <li style={marginBottom}>
+                                <a href="https://github.com/badluckxiii/pokedex" className="buttonNav separator" target="_blank" rel="noopener noreferrer">
+                                    Github repository
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div style={{ width: window.innerWidth<768?'100%':'30%', textAlign: 'right' }}>
+                <input placeholder="Search a pokemon..." style={{ marginRight: '50px', borderRadius: '15px' }} type="text" name="" id="" />
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default NavHeader
+export default NavHeader;
