484083b7ba9b46efe8c8de47c4f3b8b7e559a6a5
Josep Gregori <josepgb13@gmail.com>
2023-05-31 17:12:27 -0400

    Agregado standards de js y correcciones

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-
 const Footer = () => {
-    return (
-        <footer>
-            <h3>
-                Developed by badluckxiii
-            </h3>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <h3>Developed by badluckxiii</h3>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
